071661997da538b18cfb0ca5830cc3f6f30ddd9e
sunhe <sunhe@DESKTOP-SJSH78A>
2022-04-24 20:06:57 +0900

    220424_chap01

---
+++ Chap01/src/sec03/exam01/Hello.java
@@ -0,0 +1,12 @@
+package sec03.exam01;
+
+public class Hello {
+
+	//프로그램 실행 진입점
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, Java");
+
+	} //end of main
+	
+} //end of class
